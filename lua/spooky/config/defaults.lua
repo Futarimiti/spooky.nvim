527d4697e53c61d6e7c5a3bbd808865ffd0e3684
Futar <Futarimiti@163.com>
2023-09-30 13:39:34 +0100

    config: add field `auto_use_only`

--- lua/spooky/config/defaults.lua
+++ lua/spooky/config/defaults.lua
@@ -2,12 +2,14 @@ local M = {}
 
 M.defaults = { directory = vim.fn.stdpath('config') .. 'skeletons'
              , case_sensitive = true
+             , auto_use_only = true
              }
 
 -- Panics when the typecheck fails.
 M.typecheck = function (config)
   vim.validate { directory = { config.directory, 'string' }
                , case_sensitive = { config.case_sensitive, 'boolean' }
+               , auto_use_only = { config.auto_use_only, 'boolean' }
                }
 end
 
