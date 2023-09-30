75989a7ce6fcf7541d1f5eb5c831e718955e64bc
Futar <Futarimiti@163.com>
2023-09-30 13:38:06 +0100

    config: add field `case_sensitive`

--- lua/spooky/config/defaults.lua
+++ lua/spooky/config/defaults.lua
@@ -1,11 +1,14 @@
 local M = {}
 
 M.defaults = { directory = vim.fn.stdpath('config') .. 'skeletons'
+             , case_sensitive = true
              }
 
 -- Panics when the typecheck fails.
 M.typecheck = function (config)
-  vim.validate { directory = { config.directory, 'string' } }
+  vim.validate { directory = { config.directory, 'string' }
+               , case_sensitive = { config.case_sensitive, 'boolean' }
+               }
 end
 
 -- sanity check
