610d584650682b96d1e6af24b3e096f9727b5744
Futar <Futarimiti@163.com>
2023-10-02 17:12:03 +0100

    add option to choose select ui

--- lua/spooky/config/defaults.lua
+++ lua/spooky/config/defaults.lua
@@ -1,9 +1,12 @@
 local M = {}
 
+local const = require 'spooky.consts'
+
 M.defaults = { directory = vim.fn.stdpath('config') .. '/skeletons'
              , case_sensitive = false
              , auto_use_only = true
              , show_no_template = true
+             , ui = { select = 'builtin' }
              }
 
 -- Panics when the typecheck fails.
@@ -12,6 +15,8 @@ M.typecheck = function (config)
                , case_sensitive = { config.case_sensitive, 'boolean' }
                , auto_use_only = { config.auto_use_only, 'boolean' }
                , show_no_template = { config.show_no_template, 'boolean' }
+               , ui = { config.ui, 'table' }
+               , ['ui.select'] = { config.ui.select, function (ui) return vim.tbl_contains(const.ui_list, ui) end }
                }
 end
 
