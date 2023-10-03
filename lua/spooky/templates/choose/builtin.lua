8f5777d1caa1bf8ca6de82c3d610c25d8a6a43b7
Futar <Futarimiti@163.com>
2023-10-03 09:41:52 +0100

    impl for telescope

--- lua/spooky/templates/choose/builtin.lua
+++ lua/spooky/templates/choose/builtin.lua
@@ -6,19 +6,20 @@ M.choose_one = function (fullpaths, user, do_with_choice)
   local no_template = user.ui.no_template
   local show_full = user.ui.select_full_path
   local show_no_template = user.show_no_template
-  local representation = (function ()
-    local ret = {}
+  local representation, mappings = (function ()
+    local ret, map = {}, {}
     for _, fullpath in ipairs(fullpaths) do
       local path = show_full and fullpath or vim.fn.fnamemodify(fullpath, ':t:r')
       table.insert(ret, path)
+      map[path] = fullpath
     end
     if show_no_template then table.insert(ret, no_template) end
-    return ret
+    return ret, map
   end)()
 
-  vim.ui.select(representation, { prompt = user.ui.prompt }, function (choice, idx)
+  vim.ui.select(representation, { prompt = user.ui.prompt }, function (choice, _)
     if choice == nil or choice == no_template then return end
-    local fullpath = fullpaths[idx]
+    local fullpath = assert(mappings[choice])
     do_with_choice(fullpath)
   end)
 end
