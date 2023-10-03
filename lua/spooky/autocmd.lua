70697d30e69dd5cb2a9bd46f11b79d6b0d72e6f2
Futar <Futarimiti@163.com>
2023-10-03 11:47:41 +0100

    simplify code

--- lua/spooky/autocmd.lua
+++ lua/spooky/autocmd.lua
@@ -1,18 +1,14 @@
 local M = {}
 
-local create_callback = function (user)
-  return function (_)
-    local win = vim.api.nvim_get_current_win()
-    require('spooky.templates').maybe_insert(win, user, false)
-  end
-end
-
 M.setup_autocmds = function (user)
   local group = vim.api.nvim_create_augroup('spooky', {})
   vim.api.nvim_create_autocmd( { 'VimEnter' }
                              , { group = group
                                , pattern = '*'
-                               , callback = create_callback(user)
+                               , callback = function (_)
+                                   local win = vim.api.nvim_get_current_win()
+                                   require('spooky.templates').maybe_insert(win, user, true)
+                                 end
                                }
                              )
 end
